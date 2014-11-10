2888b738239872635c3fbe4d00bbf0b2da45ba08
Code Division <hello@11th.ventures>
2014-11-10 15:12:04 +0800

    updates

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -14,7 +14,7 @@ end
 get '/auth' do
   @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
   session.delete(:request_token)
-   user = User.create(username:@access_token.params[:screen_name], oauth_token: @access_token.token, oauth_secret: @access_token.secret )
+   twitteruser = TwitterUser.create(username:@access_token.params[:screen_name], oauth_token: @access_token.token, oauth_secret: @access_token.secret )
   session[:id] = user.id
 
 erb :index
@@ -22,7 +22,7 @@ erb :index
 end
 
 post '/auth' do
-  user = User.find(session[:id])
+  twitteruser = TwitterUser.find(session[:id])
   Twitter.configure do |config|
   config.oauth_token = user.oauth_token
   config.oauth_token_secret = user.oauth_secret
