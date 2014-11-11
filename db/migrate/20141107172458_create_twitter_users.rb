3b89de6a29ac37acba5c7e6240e0d7ddd2cc862d
Code Division <hello@11th.ventures>
2014-11-11 10:35:49 +0800

    new database

--- db/migrate/20141107172458_create_twitter_users.rb
+++ db/migrate/20141107172458_create_twitter_users.rb
@@ -2,12 +2,8 @@ class CreateTwitterUsers < ActiveRecord::Migration
   def change
   	create_table :twitter_users do |t|
       t.string :twitter_username
-      t.string :consumer_key
-      t.string :consumer_secret
       t.string :oauth_token
       t.string :oauth_token_secret
-
-      t.timestamps
 
   		t.timestamps
   	end
